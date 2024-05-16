6f25264c091512c0a01957f5565a36d383b40e67
rob thijssen <rthijssen@gmail.com>
2024-05-16 09:02:26 +0300

    extrinsics

--- src/App.js
+++ src/App.js
@@ -4,11 +4,46 @@ import Container from 'react-bootstrap/Container';
 const App = () => {
   const [data, setData] = useState(undefined);
   useEffect(() => {
-    //setInterval(() => {
+    setInterval(() => {
       fetch(`https://gnoyager1.v8r.io/graphql/query`, {
         method: 'POST',
         body: JSON.stringify({
-          query: 'query{transactions(filter:{}){block_height,hash,messages{route,typeUrl}}}'
+          query: `query {
+            transactions(filter:{}) {
+              block_height
+              hash
+              messages {
+                route,
+                typeUrl
+                value {
+                  __typename,
+                  ... on BankMsgSend {
+                    from_address
+                    to_address
+                    amount
+                  }
+                  ... on MsgAddPackage {
+                    creator
+                    package {
+                      name
+                      path
+                    }
+                  }
+                  ... on MsgCall {
+                    caller
+                    pkg_path
+                    args
+                  }
+                  ... on MsgRun {
+                    caller
+                    package {
+                      path
+                    }
+                  }
+                }
+              }
+            }
+          }`
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -23,17 +58,49 @@ const App = () => {
           transactions: transactions.map(({
             block_height: block,
             hash,
-            messages: [ { route, typeUrl: method } ],
+            messages: [
+              {
+                value,
+                value: {
+                  __typename: name,
+                }
+              }
+            ],
           }) => ({
             block,
             hash,
-            route,
-            method,
+            extrinsic: {
+              name,
+              value: {
+                ...(name === 'BankMsgSend') && {
+                  from: value.from_address,
+                  to: value.to_address,
+                  amount: value.amount,
+                },
+                ...(name === 'MsgAddPackage') && {
+                  creator: value.creator,
+                  package: value.package,
+                },
+                ...(name === 'MsgCall') && {
+                  caller: value.caller,
+                  package: {
+                    path: value.pkg_path,
+                  },
+                  args: value.args,
+                },
+                ...(name === 'MsgRun') && {
+                  caller: value.caller,
+                  package: {
+                    path: value.package.path,
+                  },
+                },
+              },
+            },
           })).reverse(),
         }));
       })
       .catch(console.error);
-    //}, 3000);
+    }, 3000);
   }, []);
   return (
     <Container fluid>
