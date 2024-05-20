7746960d315c8abe7f977565c66cdb694246204e
rob thijssen <rthijssen@gmail.com>
2024-05-20 08:28:43 +0200

    use json rpc endpoint...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+import { Message } from "protobufjs/light";
+//import { /*encodeString,*/ decodeString } from '@tendermint/amino-js';
+//import { Observable } from 'relay-runtime';
+//import { Observable } from 'rxjs';
+//import { createClient } from 'graphql-ws';
 //import graphql from "babel-plugin-relay/macro";
+/*
+const client = createClient({
+  url: 'wss://gnoyager1.v8r.io/ws',
+});
+
+const toObservable = (operation) => (
+  new Observable((observer) =>
+    client.subscribe(operation, {
+      next: (data) => observer.next(data),
+      error: (err) => observer.error(err),
+      complete: () => observer.complete(),
+    }),
+  )
+);
+*/
 
 const App = () => {
   const [data, setData] = useState(undefined);
+  const [filter, setFilter] = useState(undefined);
+  /*
   useEffect(() => {
     setInterval(() => {
       fetch(`https://gnoyager1.v8r.io/graphql/query`, {
@@ -103,6 +125,54 @@ const App = () => {
       .catch(console.error);
     }, 3000);
   }, []);
+  */
+  useEffect(() => {
+    if (!filter) {
+      fetch(`https://gnoyager1.v8r.io`, {
+        method: 'POST',
+        body: JSON.stringify({
+          id: 1,
+          jsonrpc: '2.0',
+          method: 'newBlockFilter',
+          params: []
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => response.json())
+      .then(({ result }) => setFilter(result))
+      .catch(console.error);
+    };
+  }, []);
+  useEffect(() => {
+    setInterval(() => {
+      if (!!filter) {
+        fetch(`https://gnoyager1.v8r.io`, {
+          method: 'POST',
+          body: JSON.stringify({
+            id: 1,
+            jsonrpc: '2.0',
+            method: 'getFilterChanges',
+            params: [
+              filter
+            ]
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((response) => response.json())
+        .then(({ result }) => {
+          if (!!result && !!result.length) {
+            setData(atob(result[0]).split('\n'));
+          }
+        })
+        .catch(console.error);
+      };
+    }, 3000);
+  }, [ filter ]);
+
   return (
     <Container fluid>
       {
