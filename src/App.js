e4da76982b92edb240da0628d0936f4d71f11371
rob thijssen <rthijssen@gmail.com>
2024-05-21 19:10:14 +0200

    stats

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import { useState, useEffect } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
-import { Message } from "protobufjs/light";
-//import { /*encodeString,*/ decodeString } from '@tendermint/amino-js';
-//import { Observable } from 'relay-runtime';
-//import { Observable } from 'rxjs';
-//import { createClient } from 'graphql-ws';
-//import graphql from "babel-plugin-relay/macro";
-/*
-const client = createClient({
-  url: 'wss://gnoyager1.v8r.io/ws',
-});
+import Spinner from 'react-bootstrap/Spinner';
 
-const toObservable = (operation) => (
-  new Observable((observer) =>
-    client.subscribe(operation, {
-      next: (data) => observer.next(data),
-      error: (err) => observer.error(err),
-      complete: () => observer.complete(),
-    }),
-  )
-);
-*/
+const iso_dt = (offset = 0) => {
+  return `${(new Date( +new Date() + offset )).toISOString().slice(0, -5)}Z`;
+};
 
 const App = () => {
-  const [data, setData] = useState(undefined);
-  const [filter, setFilter] = useState(undefined);
-  /*
+  const [range, setRange] = useState({size: 100});
+  const [blockchain, setBlockchain] = useState(undefined);
+  const [transactions, setTransactions] = useState(undefined);
   useEffect(() => {
     setInterval(() => {
       fetch(`https://gnoyager1.v8r.io/graphql/query`, {
         method: 'POST',
         body: JSON.stringify({
           query: `query {
-            transactions(filter:{}) {
+            blocks(
+              filter: {
+                from_time: "${iso_dt(-30000)}"
+              }
+            ) {
+              height
+              version
+              chain_id
+              time
+              proposer_address_raw
+            }
+          }`
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => response.json())
+      .then(({ data: { blocks } }) => {
+        const b = blocks.slice(-1)[0];
+        setBlockchain({
+          id: b.chain_id,
+          version: b.version,
+          last: {
+            block: {
+              time: b.time,
+              number: b.height,
+              proposer: b.proposer_address_raw,
+            }
+          }
+        })
+      })
+      .catch(console.error);
+    }, 1000);
+  }, []);
+  useEffect(() => {
+    if (!!blockchain && !!blockchain.last && !!blockchain.last.block && !!blockchain.last.block.number) {
+      fetch(`https://gnoyager1.v8r.io/graphql/query`, {
+        method: 'POST',
+        body: JSON.stringify({
+          query: `query {
+            transactions(
+              filter:{
+                from_block_height: ${Math.max(1, (blockchain.last.block.number - (range.size - 1)))}
+              }
+            ) {
               block_height
               hash
               messages {
@@ -73,12 +103,9 @@ const App = () => {
         }
       })
       .then((response) => response.json())
-      .then(({ data: { transactions } }) => {
-        const height = transactions.slice(-1)[0].block_height;
-        setData((data) => ({
-          ...data,
-          height,
-          transactions: transactions.map(({
+      .then(({data}) => {
+        if (!!data && !!data.transactions) {
+          setTransactions(data.transactions.map(({
             block_height: block,
             hash,
             messages: [
@@ -88,98 +115,147 @@ const App = () => {
                   __typename: name,
                 }
               }
-            ],
-          }) => ({
-            block,
-            hash,
-            extrinsic: {
-              name,
-              value: {
-                ...(name === 'BankMsgSend') && {
-                  from: value.from_address,
-                  to: value.to_address,
-                  amount: value.amount,
-                },
-                ...(name === 'MsgAddPackage') && {
-                  creator: value.creator,
-                  package: value.package,
-                },
-                ...(name === 'MsgCall') && {
-                  caller: value.caller,
-                  package: {
-                    path: value.pkg_path,
+            ]}) => ({
+              block,
+              hash,
+              sender: (
+                (name === 'BankMsgSend')
+                  ? value.from_address
+                  : (name === 'MsgAddPackage')
+                    ? value.creator
+                    : (['MsgCall', 'MsgRun'].includes(name))
+                      ? value.caller
+                      : undefined
+              ),
+              extrinsic: {
+                name,
+                value: {
+                  ...(name === 'BankMsgSend') && {
+                    from: value.from_address,
+                    to: value.to_address,
+                    amount: value.amount,
                   },
-                  args: value.args,
-                },
-                ...(name === 'MsgRun') && {
-                  caller: value.caller,
-                  package: {
-                    path: value.package.path,
+                  ...(name === 'MsgAddPackage') && {
+                    creator: value.creator,
+                    package: value.package,
+                  },
+                  ...(name === 'MsgCall') && {
+                    caller: value.caller,
+                    package: {
+                      path: value.pkg_path,
+                    },
+                    args: value.args,
+                  },
+                  ...(name === 'MsgRun') && {
+                    caller: value.caller,
+                    package: {
+                      path: value.package.path,
+                    },
                   },
                 },
               },
-            },
-          })).reverse(),
-        }));
-      })
-      .catch(console.error);
-    }, 3000);
-  }, []);
-  */
-  useEffect(() => {
-    if (!filter) {
-      fetch(`https://gnoyager1.v8r.io`, {
-        method: 'POST',
-        body: JSON.stringify({
-          id: 1,
-          jsonrpc: '2.0',
-          method: 'newBlockFilter',
-          params: []
-        }),
-        headers: {
-          'Content-Type': 'application/json'
+            })).reverse()
+          );
         }
+        /*
+        */
       })
-      .then((response) => response.json())
-      .then(({ result }) => setFilter(result))
       .catch(console.error);
-    };
-  }, []);
-  useEffect(() => {
-    setInterval(() => {
-      if (!!filter) {
-        fetch(`https://gnoyager1.v8r.io`, {
-          method: 'POST',
-          body: JSON.stringify({
-            id: 1,
-            jsonrpc: '2.0',
-            method: 'getFilterChanges',
-            params: [
-              filter
-            ]
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then((response) => response.json())
-        .then(({ result }) => {
-          if (!!result && !!result.length) {
-            setData(atob(result[0]).split('\n'));
-          }
-        })
-        .catch(console.error);
-      };
-    }, 3000);
-  }, [ filter ]);
+    }
+  }, [blockchain, range]);
 
   return (
-    <Container fluid>
+    <Container>
       {
-        (!!data)
+        (!!blockchain)
+          ? (
+              <Alert variant="warning">
+                statistics shown relate to extrinsics executed between block {Math.max(1, (blockchain.last.block.number - (range.size - 1)))} and {blockchain.last.block.number} inclusive.
+              </Alert>
+            )
+          : (
+              <Spinner animation="border" size="sm" />
+            )
+      }
+      <ul>
+        <li>
+          number of transactions: {
+            (!!transactions)
+              ? (transactions.length)
+              : (
+                  <Spinner animation="border" size="sm" />
+                )
+          }
+        </li>
+        <li>
+          number of different message types: {
+            (!!transactions)
+              ? (
+                  <ul>
+                    {
+                      [...new Set(transactions.map((tx) => tx.extrinsic.name))].map((extrinsic) => (
+                        <li key={extrinsic}>
+                          {extrinsic}: {transactions.filter((tx) => (tx.extrinsic.name === extrinsic)).length}
+                        </li>
+                      ))
+                    }
+                  </ul>
+                )
+              : (
+                  <Spinner animation="border" size="sm" />
+                )
+          }
+        </li>
+        <li>
+          most active accounts: {
+            (!!transactions)
+              ? (
+                  <ul>
+                    {
+                      [...new Set(transactions.map((tx) => tx.extrinsic.name))].map((extrinsic) => (
+                        <li key={extrinsic}>
+                          {extrinsic}:
+                          <ul>
+                            {
+                              [...new Set(transactions.filter((tx) => (tx.extrinsic.name === extrinsic)).map(({sender}) => sender))].map((sender) => (
+                                <li key={sender}>
+                                  <pre style={{display: 'inline'}}>{sender}</pre>: {
+                                    transactions.filter((tx) => (tx.extrinsic.name === extrinsic && tx.sender === sender)).length
+                                  }
+                                </li>
+                              ))
+                            }
+                          </ul>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                )
+              : (
+                  <Spinner animation="border" size="sm" />
+                )
+          }
+        </li>
+        {
+          /*
+        <li>most active realms / packages deployed / called</li>
+          */
+        }
+      </ul>
+      {
+        (!!transactions)
           ? (
               <pre>
-                {JSON.stringify(data, null, 2)}
+                {JSON.stringify(transactions, null, 2)}
+              </pre>
+            )
+          : null
+      }
+      {
+        (!!blockchain)
+          ? (
+              <pre>
+                {JSON.stringify(blockchain, null, 2)}
               </pre>
             )
           : null
--- src/Block.js
+++ src/Block.js
@@ -1,34 +1,38 @@
 import { Observable } from 'relay-runtime';
-// or
-import { Observable } from '@apollo/client/core';
-// or
-import { Observable } from 'rxjs';
-// or
-import Observable from 'zen-observable';
-// or any other lib which implements Observables as per the ECMAScript proposal: https://github.com/tc39/proposal-observable
- 
+
 const client = createClient({
-  url: 'ws://graphql.loves:4000/observables',
+  url: 'wss://gnoyager1.v8r.io/graphql/query',
 });
- 
-function toObservable(operation) {
-  return new Observable((observer) =>
+
+const toObservable = (operation) => (
+  new Observable((observer) =>
     client.subscribe(operation, {
       next: (data) => observer.next(data),
       error: (err) => observer.error(err),
       complete: () => observer.complete(),
     }),
   );
-}
- 
-const observable = toObservable({ query: `subscription { ping }` });
- 
+);
+
+const observable = toObservable({
+  query: `
+    subscription {
+      blocks(filter: {}) {
+        height
+        version
+        chain_id
+        time
+        proposer_address_raw
+      }
+    }
+`
+});
 const subscription = observable.subscribe({
   next: (data) => {
-    expect(data).toBe({ data: { ping: 'pong' } });
+    //expect(data).toBe({ data: { ping: 'pong' } });
   },
 });
- 
+
 // ⏱
- 
+
 subscription.unsubscribe();
